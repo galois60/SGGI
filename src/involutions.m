bd2b0b1443d4a5f78b3d805d50635d4c245e87e7
galois60 <pbrooksb@bucknell.edu>
2018-08-30 14:36:18 -0400

    Update involutions.m

--- src/involutions.m
+++ src/involutions.m
@@ -25,3 +25,56 @@ intrinsic WhichClass (G::GrpPerm, C::SeqEnum, x::GrpPermElt) -> RngIntElt
        return 0;
   end if;
 end intrinsic;
+
+
+intrinsic ClassProfile (G::Grp, R::SeqEnum, T::SeqEnum) -> SeqEnum 
+  { Given a list R of G-conjugacy class representatives, and a list
+    T of elements of G, return the positions of the elements of R
+    to which elements of T are G-conjugate. }
+  pos := [ ];
+  for i in [1..#T] do
+      flag, n := WhichClass (G, R, T[i]);
+      require flag : "elements of T must be conjugate to reps in R";
+      Append (~pos, n);
+  end for;   
+return pos;
+end intrinsic;
+
+
+// given H acting by conjugation on class, return reps of H-classes
+intrinsic RefineClass (H::Grp, C::SeqEnum) -> SeqEnum
+  { Replace the H-invariant list C with a (possibly) smaller list 
+    of representatives under the H-action. }
+     if #C eq 0 then 
+         return [ ];
+     end if;
+     assert exists (r){ s : s in C };
+     ref := [ r ];
+     nclass := Conjugates (H, r);
+     while #nclass lt #C do
+         assert exists (r){ s : s in C | not s in nclass };
+         nclass join:= Conjugates (H, r);
+         Append (~ref, r);
+     end while;
+return ref;
+end intrinsic;
+
+intrinsic OrbitReps (G::Grp, S::Set) -> SeqEnum, SeqEnum
+  { Given a group G acting on a set S, find representatives of G-orbits };
+  if #S eq 0 then
+       return [ ];
+  end if;
+  assert exists (s){ x : x in S };
+  R := [ s ];
+  T := Orbit (G, s);
+  sizes := [ #T ];
+  while #T lt #S do
+       assert exists (s){ x : x in S | not x in T };
+       O := Orbit (G, s);
+       T join:= O;
+       Append (~R, s);
+       Append (~sizes, #O);
+  end while;
+return R, sizes;
+end intrinsic;
+
