050afec7432cfd1c67ba07e2d2adc9d7a341bbaf
Peter Brooksbank <pbrooksb@bucknell.edu>
2020-03-08 11:02:32 -0400

    minor changes

--- experiments/sym.m
+++ experiments/sym.m
@@ -6,63 +6,81 @@
   Define the following terms:
      ISLAND = any SGGI that isn't a reduction and cannot be reduced
      ROOT = any SGGI of rank [3..n-3] that isn't a reduction
-     LEAF = any SGGI of rank [4..n-2]
+     LEAF = any SGGI of rank [4..n-2] that does not reduce
+     NORMAL = all remaining SGGIs
 */
+
+__Process := function (S)
+  N := {@ @};   // the normal SC-reps
+  I := {@ @};   // the islands
+  R := {@ @};   // the roots
+  L := {@ @};   // the leaves
+  // set up the loop variables
+  A := IndexedSet (S[1]);   // all nodes on level 1 are aliens
+  C := {@ @};               // no nodes on level 1 are children
+  i := 1;
+  while i lt #S do
+    C_new := {@ @};
+    T := IndexedSet (S[i+1]);
+    // process first the children on level i
+    for H in C do
+      // consider the two reductions of H
+      HL := LeftPetrie (H);
+      HR := RightPetrie (H);
+      Lflag := (#Group(HL) eq #Group(H)) and HasIntersectionProperty (HL);
+      Rflag := (#Group(HR) eq #Group(H)) and HasIntersectionProperty (HR);
+      if Lflag then   
+          assert exists (J){ K : K in T | IsEquivalent (HL, K) };
+          Include (~N, H);
+          Include (~C_new, J);
+      end if;
+      if Rflag then   
+          assert exists (J){ K : K in T | IsEquivalent (HR, K) };
+          Include (~N, H);
+          Include (~C_new, J);
+      end if;
+      if (not Lflag) and (not Rflag) then
+          Include (~L, H);
+      end if;
+    end for;
+    // now process the aliens on level i
+    for H in A do
+      HL := LeftPetrie (H);
+      HR := RightPetrie (H);
+      Lflag := (#Group(HL) eq #Group(H)) and HasIntersectionProperty (HL);
+      Rflag := (#Group(HR) eq #Group(H)) and HasIntersectionProperty (HR);
+      if Lflag then   
+          assert exists (J){ K : K in T | IsEquivalent (HL, K) };
+          Include (~R, H);
+          Include (~C_new, J);
+      end if;
+      if Rflag then   
+          assert exists (J){ K : K in T | IsEquivalent (HR, K) };
+          Include (~R, H);
+          Include (~C_new, J);
+      end if;
+      if (not Lflag) and (not Rflag) then
+          Include (~I, H);
+      end if;
+    end for;
+    C := C_new;
+    A := T diff C;
+    i +:= 1;
+  end while;
+
+  // now classify the rank 3 SC-reps into "normal" and "islands"
+  T := IndexedSet (S[#S]);
+  N join:= C;
+  I join:= (T diff C);
+
+return I, L, N, R;
+end function;
+
 
 n := 8;
 G := SymmetricGroup (n);
 
 ALL := [ AllStringCReps (G, r) : r in [n-2..3 by -1] ];
-assert #ALL[1][1] eq 1;
+assert #ALL[1] eq 1;
 
 
-ROOTS := { ALL[1][1] };
-
-
-
-for i in [1..n-4] do
-  Ei := [ ];
-  Ai := ALL[i];
-  for j in [1..#Ai] do
-    H := Ai[j];
-    HL := LeftPetrie (H);
-    if HasIntersectionProperty (HL) and (#Group(HL) eq #Group(H)) then
-         assert exists (k){ l : l in [1..#ALL[i+1]] | IsEquivalent (HL, ALL[i+1][l]) };
-         Append (~Ei, [j, k]);
-    end if;
-    HR := RightPetrie (H);
-    if HasIntersectionProperty (HR) and (#Group(HR) eq #Group(H)) then
-         assert exists (k){ l : l in [1..#ALL[i+1]] | IsEquivalent (HR, ALL[i+1][l]) };
-        Append (~Ei, [j, k]);
-    end if;
-  end for;
-  Append (~edges, Ei);
-end for;
-edges;
-
-__PROFILE := function (E, nums)
-  roots := [ ];
-  leaves := [ ];
-  islands := [ ];
-  for i in [1..#nums] do
-    for j in [1..nums[i]] do
-      // is [i,j] a root, a leaf, both (i.e. an island) or neither?
-      if (i eq 1) then
-           Rij := true;
-      else
-           Rij := not exists { e : e in E[i-1] | e[2] eq j };
-      end if;
-      Lij := not exists { e : e in E[i] | e[1] eq j };
-      if Rij then
-        if Lij then
-          Append (~islands, [i,j]);
-        else 
-          Append (~roots, [i,j]);
-        end if;
-      elif Lij then
-        Append (~leaves, [i,j]);
-      end if;
-    end for;
-  end for;
-return roots, leaves, islands;
-end function;
